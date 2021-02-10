c0e153e529b044e70c886db407f2beee7efb8e42
sheriff olowolagba <sh3riff101@gmail.com>
2021-02-10 08:07:49 +0100

    Create Charlie.js

---
+++ src/Charlie.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Charlie App</h1>
+      <h2>Welcome to Charlie App</h2>
+    </div>
+  );
+}
